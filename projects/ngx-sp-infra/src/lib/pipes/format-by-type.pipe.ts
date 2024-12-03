f8bd60ce9540fe306c58969e8ece92f97e90076d
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-12-03 16:21:14 -0300

    v3.8.2-next | Commit automático
    Commit automático realizado via script pós build

--- projects/ngx-sp-infra/src/lib/pipes/format-by-type.pipe.ts
+++ projects/ngx-sp-infra/src/lib/pipes/format-by-type.pipe.ts
@@ -9,19 +9,8 @@ export class FormatByTypePipe implements PipeTransform {
     if (typeof value === 'string' || typeof value === 'number') { return value.toString(); }
 
     if (value instanceof Date) {
-      const dataObj = new Date(value); // Cria um objeto Date a partir da string
-      const dia = dataObj.getDate().toString().padStart(2, '0'); // Obtém o dia e adiciona zero à esquerda se necessário
-      const mes = (dataObj.getMonth() + 1).toString().padStart(2, '0'); // Obtém o mês (lembrando que o mês começa de 0)
-      const ano = dataObj.getFullYear(); // Obtém o ano
-      
-      return `${dia}/${mes}/${ano}`; // Retorna no formato desejado
-      
-      // Usando Intl.DateTimeFormat para formatar datas
-      // return new Intl.DateTimeFormat('pt-BR', {
-      //   day: '2-digit',
-      //   month: '2-digit',
-      //   year: 'numeric',
-      // }).format(value);
+      const dataObj = new Date(value);
+      return dataObj.toLocaleString();
     }
 
     if (typeof value === 'boolean') { return value ? 'Sim' : 'Não'; }
