a55e5703afaee0888f9420a990a80de0edcc2c36
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-12-10 10:41:28 -0300

    v4.1.4-test | Commit automático
    Commit automático realizado via script pós build

--- projects/ngx-sp-infra/src/lib/pipes/format-by-type.pipe.spec.ts
+++ projects/ngx-sp-infra/src/lib/pipes/format-by-type.pipe.spec.ts
@@ -5,12 +5,12 @@ describe('Pipe de formatação de texto por tipo', () => {
    // This pipe is a pure, stateless function so no need for BeforeEach
    const pipe = new FormatByTypePipe();
  
-   it('quando filtro do tipo "Date()" estiver presente, deve formatá-lo para "00/00/0000"', () => {
+   it('quando filtro do tipo "Date("Tue Dec 10 2024 10:36:26 GMT-0300 (Horário Padrão de Brasília)")" estiver presente, deve formatá-lo para "10/12/2024"', () => {
       const values = [
-        { label: 'Filtro de data:', value: new Date() }
+        { label: 'Filtro de data:', value: new Date("Tue Dec 10 2024 10:36:26 GMT-0300 (Horário Padrão de Brasília)") }
       ];
       
-      expect(pipe.transform(values[0].value)).toBe('09/12/2024');
+      expect(pipe.transform(values[0].value)).toBe('10/12/2024');
    });
 
    it('quando filtro do tipo "string" com valor "2024-12-01" estiver presente, deve formatá-lo para "01/12/2024"', () => {
