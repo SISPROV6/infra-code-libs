0ef99ac4eb3cf46b399e6408e37d27df5501ea94
Erick Carvalho <erickzcarvalho25@outlook.com>
2024-12-19 14:34:30 -0300

    v3.10.4 | Commit automático
    Commit automático realizado via script pós build

--- projects/ngx-sp-infra/src/lib/widgets/custom-acordion/custom-acordion.component.ts
+++ projects/ngx-sp-infra/src/lib/widgets/custom-acordion/custom-acordion.component.ts
@@ -1,43 +1,61 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
+/**
+ * Componente de acordion customizado, o seu conteúdo interno é informado pelo componente pai, podendo configurá-lo como necessário.
+ * O componente em si é responsável apenas pelo comportamento de acordion, ou seja, abrir e fechar o conteúdo interno.
+ * 
+ * Propriedades:
+ *    - name: string (default: 'Filtro avançado')
+ *    - haveArrow: boolean (default: true)
+ *    - haveMarginTop: boolean (default: false)
+ *    - isOpen: boolean (default: false)
+ */
 @Component( {
       selector: 'app-custom-acordion, lib-acordion',
       templateUrl: './custom-acordion.component.html',
       styleUrls: [ './custom-acordion.component.scss' ],
 } )
 export class CustomAcordionComponent implements OnInit {
-      constructor () { }
-      customClass = 'customClass';
 
-      @Input() public name = 'Filtro avançado';
-      @Input() public haveArrow = true;
-      @Input() public haveMarginTop = false;
+      // #region ==========> PROPERTIES <==========
+
+      // #region PUBLIC
+      @Input() public name: string = 'Filtro avançado';
+      @Input() public haveArrow: boolean = true;
+      @Input() public haveMarginTop: boolean = false;
+
       @Input() public isOpen?: boolean;
+      @Input() public customClass?: string;
 
-      public posicaoIcon?:boolean; 
+      @ViewChild('icon') iconRef?: ElementRef;
 
-	@ViewChild('icon') iconRef?: ElementRef;
+      protected iconDirection: 'baixo' | 'cima' = 'baixo';
+      // #endregion PUBLIC
 
-      ngOnInit (): void {       
-            this.posicaoIcon = this.isOpen;
+      // #endregion ==========> PROPERTIES <==========
 
-            this.toogleIcon();
-      }
 
-      public setIsOpen(){
+      // #region ==========> INITIALIZATION <==========
+      constructor() { }
+
+      ngOnInit(): void {       
+            //this.posicaoIcon = this.isOpen;
             
-            this.toogleIcon();
+            if (this.isOpen) this.iconDirection = 'baixo';
+            else this.iconDirection = 'cima';
+      }
+      // #endregion ==========> INITIALIZATION <==========
 
-            this.posicaoIcon = !this.posicaoIcon;
+
+      // #region ==========> UTILS <==========
+      public setIsOpen(){
+            this.toogleIcon();
       }
 
       public toogleIcon(){
-            if(this.iconRef){
-                  if(this.posicaoIcon){
-                        this.iconRef.nativeElement.classList.add('invertImage');
-                  } else {
-                        this.iconRef.nativeElement.classList.remove('invertImage');
-                  }
-            }
+            if (this.iconDirection === 'baixo') this.iconDirection = 'cima';
+            else this.iconDirection = 'baixo';
       }
+      // #endregion ==========> UTILS <==========
+
 }
