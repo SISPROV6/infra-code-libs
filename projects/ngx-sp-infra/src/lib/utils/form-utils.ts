e3c89b030491527e8810d1e93c0f7a21be862c8d
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-02-26 15:32:22 -0300

    bkp

--- projects/ngx-sp-infra/src/lib/utils/form-utils.ts
+++ projects/ngx-sp-infra/src/lib/utils/form-utils.ts
@@ -44,7 +44,7 @@ export class FormUtils {
       if (control instanceof UntypedFormGroup || control instanceof UntypedFormArray) this.validateFields(control);
     });
   }
-      
+
   /** Obtém a mensagem de erro conforme o validador utilizado */
   static getErrorMessage(validatorName: string, validatorValue?: any, customErrorMessage?: string, fieldName?: string): string {
     const config : any = {
@@ -75,9 +75,9 @@ export class FormUtils {
    * @summary Mapeia os valores de um formulário reativo para um objeto de modelo.
    *
    * @description
-   * O método `mapFormToModel` recebe um objeto de modelo e um `FormGroup` do Angular, 
-   * e retorna um novo objeto que combina as propriedades do modelo original com os valores 
-   * atuais do formulário. Este método é útil para atualizar dinamicamente as propriedades 
+   * O método `mapFormToModel` recebe um objeto de modelo e um `FormGroup` do Angular,
+   * e retorna um novo objeto que combina as propriedades do modelo original com os valores
+   * atuais do formulário. Este método é útil para atualizar dinamicamente as propriedades
    * de um modelo com base nos valores inseridos pelo usuário em um formulário.
    * 
    * Se o objeto `record` ou o `formGroup` forem nulos ou indefinidos, o método retornará `null`.
