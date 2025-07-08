3924d158d77e92b7cb489b2efa98f52015338537
vrmuller <vrmuller@sispro.com.br>
2025-07-08 14:39:53 -0300

    ngx-sp-infra | v5.0.8 | Commit automático
    Corrigido erro no caminho relativo da rota para validações de qual modulo está sendo acessado

--- projects/ngx-sp-infra/src/lib/components/logs-email/components/home-log-email/home-log-email.component.ts
+++ projects/ngx-sp-infra/src/lib/components/logs-email/components/home-log-email/home-log-email.component.ts
@@ -45,7 +45,7 @@ constructor(
 		private _tenantService: TenantService,
 		private _messageService:MessageService,
 		private _router: Router) {
-		this.module = window.location.hostname.includes('Corporativo') ? "Corporativo" : "ConfigErp";
+		this.module = window.location.href.includes('Corporativo') ? "Corporativo" : "ConfigErp";
 		if (this.module == 'ConfigErp' && (!this._tenantService.tenantId || this._tenantService.tenantId == 0)) {
 
 			this._messageService.showAlertInfo("Você deve selecionar um domínio para executar esta opção.")
--- projects/ngx-sp-infra/src/lib/components/logs-timer/components/detalhes-log-timer/detalhes-log-timer.component.ts
+++ projects/ngx-sp-infra/src/lib/components/logs-timer/components/detalhes-log-timer/detalhes-log-timer.component.ts
@@ -23,7 +23,7 @@ export class DetalhesLogTimerComponent implements OnInit {
     private _router: Router,
     private _route: ActivatedRoute,
   ) {
-    this.module = window.location.hostname.includes('Corporativo') ? "Corporativo" : "ConfigErp";
+    this.module = window.location.href.includes('Corporativo') ? "Corporativo" : "ConfigErp";
     if (this.module == 'ConfigErp' && (!this._tenantService.tenantId || this._tenantService.tenantId == 0)) {
 
       this._messageService.showAlertInfo("Você deve selecionar um domínio para executar esta opção.")
