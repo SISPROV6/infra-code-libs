558cb35eb93773aba33e103365f33bf872c83e2b
MATHEUS Cristian Dall'Alba de ALMEIDA <mcalmeida@sispro.com.br>
2025-08-29 14:49:44 -0300

    feat: ts finalizado.

--- projects/ngx-sp-infra/src/lib/views/lib-smtp-config/components/lib-smtp-config.component.ts
+++ projects/ngx-sp-infra/src/lib/views/lib-smtp-config/components/lib-smtp-config.component.ts
@@ -11,6 +11,7 @@ import { MessageService } from "../../../message/message.service";
 import { ModalUtilsService } from "../../../service/modal-utils.service";
 import { FormUtils } from "../../../utils/form-utils";
 import { SmtpConfigService } from "../services/smtp-config.service";
+import { EmailConfigTestModel } from "../models/EmailConfigTestModel";
 
 @Component({
   selector: 'lib-smtp-config',
@@ -38,6 +39,7 @@ export class LibSmtpConfigComponent {
   }
 
   ngOnInit(): void {
+    this._tenantId = this._tenantService.tenantId;
     if (this.module == 'Corporativo')
     {
       this.getParmsFromRoute();
@@ -48,7 +50,6 @@ export class LibSmtpConfigComponent {
     }
     else
     {
-      this._tenantId = this._tenantService.tenantId;
       this.getInfraEmail();
     }
   }
@@ -65,6 +66,7 @@ export class LibSmtpConfigComponent {
 
   public module: 'ConfigErp' | 'Corporativo';
 
+  //#region FORM
   public get FormUtils(): typeof FormUtils { return FormUtils }
 
   public form: FormGroup = new FormGroup({
@@ -90,7 +92,9 @@ export class LibSmtpConfigComponent {
     DESTINATARIOS: new FormControl<string>("", [Validators.required]),
     CC: new FormControl<string>("", [Validators.required]),
   })
+  //#endregion
 
+  //#region GET
   public getInfraEmail(infraEstabId: string = ""): void {
     this._smtpConfigService.GetInfraEmail(this.module,this._tenantId,infraEstabId).subscribe({
       next: response => {
@@ -113,10 +117,122 @@ export class LibSmtpConfigComponent {
           this.getServerURL();
         }
       },
-      error: error => {
-        this._messageService.showAlertDanger(error);
-      }
+      error: error => { this._messageService.showAlertDanger(error); }
     });
+  }
+  //#endregion
+
+  //#region POST
+  public SalvarCfgEmail(): void {
+    if ((this.form.valid && this.editingMode) || (this.form.valid && this.senhaForm.valid && !this.editingMode)) {
+      this.emailData = this.form.getRawValue() as InfraEmailCfgRecord;
+      this.emailData.Tenant_Id = this._tenantId
+      this.emailData.Id = this._infraEmailId;
+
+      if(this.module === 'Corporativo' && this.isInfraStab)
+      {
+        this.emailData.InfraEstabId = this._infraEstabelecimentoID
+      }
+
+      if (!this.editingMode) {
+        this.emailData.Password_Ang = this.senhaForm.get('Password_Ang')?.value;
+      }
+
+      this._smtpConfigService.SalvarCfgEmail(this.module,this.emailData).subscribe({
+        next: () => {
+          this._messageService.showAlertSuccess('Configurações de e-mail registradas com sucesso.');
+          this.getInfraEmail()
+        },
+        error: error => { this._messageService.showAlertDanger(error); }
+      });
+    }
+    else { FormUtils.validateFields(this.form) }
+  }
+
+  public UpdateSenha() {
+    if (this.senhaForm.valid) {
+      this._smtpConfigService.UpdateSenha(this.module, this.senhaForm.get('Password_Ang')?.value, this._infraEmailId).subscribe({
+        next: () => {
+          this._messageService.showAlertSuccess("Senha alterada com sucesso.")
+          this.modalUtils.closeModal(1);
+          this.senhaForm.reset();
+          this.getInfraEmail(this._infraEstabelecimentoID);
+        }, error: error => { this._messageService.showAlertDanger(error);}
+      });
+    }
+  }
+
+  public EnviarEmail() {
+    const destinatarioRawValue: string = this.mailTestForm.get("DESTINATARIOS")?.value;
+    const ccsRawValue: string = this.mailTestForm.get("CC")?.value;
+
+    let destinatarios: string[] = destinatarioRawValue.split(';');
+    let ccs: string[] = ccsRawValue.split(';');
+
+    let isDestinatariosValid: boolean = this.testEmailIsValid(destinatarios);
+    let isCcsValid: boolean = this.testEmailIsValid(ccs);
+
+
+    if(isDestinatariosValid && isCcsValid && this.editingMode){
+    let emailList:string = this.normalizeEmailList(destinatarios);
+    let ccsList: string = this.normalizeEmailList(ccs);
+
+      let record: EmailConfigTestModel = new EmailConfigTestModel();
+      record.domain = this._tenantService.dominio;
+      record.user = this.form.get("NomeRemet")?.value;
+      record.to = emailList;
+      record.cc = ccsList;
+      record.userName = this.form.get("NomeRemet")?.value;
+      record.urlDoServidor = this.form.get("UrlServidor")?.value;
+      record.code = "";
+
+      this._smtpConfigService.SendEmailConfigTest(this.module,record).subscribe({
+        next: () =>{
+          this._messageService.showAlertSuccess("Enviamos um e-mail para os destinatários escohidos! Verifique a caixa de entrada para garantir que tudo está correto (certifique-se de verificar também o seu lixo eletrônico).");
+          this.modalUtils.closeModal(2)
+        }, error: error=>{ this._messageService.showAlertDanger(error);}
+      });
+    }
+  }
+  //#endregion
+
+  //#region UTILS
+  public normalizeEmailList(destinatarios: string[]): string{
+    let emailList: string = "";
+    for (let index = 0; index < destinatarios.length; index++) {
+      emailList += destinatarios[index].trim() + (index < destinatarios.length -1 ? ";": "");
+    }
+    return emailList;
+  }
+
+  public testEmailIsValid(destinatarios: string[]): boolean{
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+    for (let index = 0; index < destinatarios.length; index++) {
+      destinatarios[index].trim();
+      if(!emailRegex.test(destinatarios[index].trim())){
+        this._toastrService.error("O seguinte e-mail não está no formato correto: " + destinatarios[index]);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public onCheck(action: 'Nenhum' | 'SSL' | 'TLS') {
+    switch (action) {
+      case 'Nenhum':
+        this.form.get('Is_SSL')?.setValue(false);
+        this.form.get('Is_TLS')?.setValue(false);
+        break;
+      case 'SSL':
+        this.form.get('Is_SSL')?.setValue(true);
+        this.form.get('Is_TLS')?.setValue(false);
+        break;
+      case 'TLS':
+        this.form.get('Is_SSL')?.setValue(false);
+        this.form.get('Is_TLS')?.setValue(true);
+        break
+    }
   }
 
   private getServerURL() 
@@ -130,4 +246,7 @@ export class LibSmtpConfigComponent {
     const id: any = this._route.snapshot.paramMap.get('id');
     this._infraEstabelecimentoID = id;
   }
+
+  public returnToList() { this._router.navigate(["/estabelecimentos"]) }
+  //#endregion
 }
