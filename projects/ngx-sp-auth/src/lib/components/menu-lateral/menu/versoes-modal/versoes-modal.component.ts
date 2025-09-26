a97f9e01ffd7a9ff1325b5c349fbb0d769523029
Marcelo Molina <mmmartins@sispro.com.br>
2025-09-26 15:43:15 -0300

    Implementação do Controle de Versões do Corporativo e Infra

---
+++ projects/ngx-sp-auth/src/lib/components/menu-lateral/menu/versoes-modal/versoes-modal.component.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { InfraModule } from 'ngx-sp-infra';
+
+import { ProjectUtilservice } from '../../../../project/project-utils.service';
+import { MenuServicesService } from '../../menu-services.service';
+
+@Component({
+    selector: 'versoes-modal',
+    templateUrl: './versoes-modal.component.html',
+    styleUrls: ['./versoes-modal.component.scss'],
+    imports: [
+        InfraModule,
+        CommonModule
+    ]
+})
+export class VersoesModalComponent implements OnInit {
+  constructor(
+    private _menuServicesService: MenuServicesService,
+    private _projectUtilService: ProjectUtilservice
+  ) { }
+
+  ngOnInit(): void {
+    this.getVersions();
+  }
+
+  // #region ==========> PROPERTIES <==========
+
+  // #region PUBLIC
+  @Output() public onClose = new EventEmitter<any>();
+  
+  public versionInfra: string = '';
+  public versionCorporativo: string = '';
+
+  // #endregion PUBLIC
+
+  // #endregion ==========> PROPERTIES <==========
+
+  // #region ==========> SERVICES <==========
+
+  // #region PREPARATION
+
+  private getVersions(): void {
+    
+    // Obtém Versão da Infra
+    this._menuServicesService.getVersionInfra().subscribe({
+      next: response => {
+        this.versionInfra = response.Version;
+      },
+      error: error => {
+        this._projectUtilService.showHttpError(error);
+      }
+    })
+
+    // Obtém Versão do Corporativo
+    this._menuServicesService.getVersionCorporativo().subscribe({
+      next: response => {
+        this.versionCorporativo = response.Version;
+      },
+      error: error => {
+        this._projectUtilService.showHttpError(error);
+      }
+    })
+
+  }
+
+  // #endregion PREPARATION
+
+  // #endregion ==========> SERVICES <==========
+
+  // #region ==========> MODALS <==========
+
+  public closeSelf() {
+    this.onClose.emit();
+  }
+
+  // #endregion ==========> MODALS <==========
+
+}
