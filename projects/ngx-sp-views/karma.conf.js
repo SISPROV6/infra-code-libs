1343fa796bcb4145662a113d0b92df06be26df30
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-09-30 18:07:19 -0300

    Ajuste por todo o projeto para comportar uma liberação automática deste novo projeto também

---
+++ projects/ngx-sp-views/karma.conf.js
@@ -0,0 +1,45 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular/cli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular/cli/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // deixa os resultados visíveis no navegador
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, '../../coverage/ngx-sp-views'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' },
+      ]
+    },
+    reporters: [ 'progress', 'kjhtml', 'spec', 'coverage' ],
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: true,
+    colors: true
+  });
+};
