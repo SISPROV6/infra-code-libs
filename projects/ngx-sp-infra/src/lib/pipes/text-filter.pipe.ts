5e4dbf1d1a8971d59ad8ddc58f79f9b726420b3c
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-10-10 15:09:52 -0300

    fix: pesquisa removida da filtragem por ID

--- projects/ngx-sp-infra/src/lib/pipes/text-filter.pipe.ts
+++ projects/ngx-sp-infra/src/lib/pipes/text-filter.pipe.ts
@@ -11,8 +11,7 @@ export class TextFilterPipe implements PipeTransform {
   transform(options: RecordCombobox[], search: string): any[] {
     if ((!options || options.length <= 0) || !search) return options;
     
-    return options.filter(e => e.LABEL.toLocaleLowerCase().includes(search.toLocaleLowerCase()) 
-      || e.ID.toLocaleString().toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    return options.filter(e => e.LABEL.toLocaleLowerCase().includes(search.toLocaleLowerCase())
       || (e.AdditionalStringProperty1 && e.AdditionalStringProperty1.toLocaleString().toLocaleLowerCase().includes(search.toLocaleLowerCase()))
       || (e.AdditionalStringProperty2 && e.AdditionalStringProperty2.toLocaleString().toLocaleLowerCase().includes(search.toLocaleLowerCase()))
     );
