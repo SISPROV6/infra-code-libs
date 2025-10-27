7215b9a038e4d83268d246fba592e5fe43ca6459
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-10-27 12:55:07 -0300

    ngx-sp-infra | v6.4.5 | Commit automático
    Ajustes finais para testes com o componente de pesquisa global de telas

--- projects/ngx-sp-auth/src/lib/models/ret-telas-pesquisa.model.ts
+++ projects/ngx-sp-auth/src/lib/models/ret-telas-pesquisa.model.ts
@@ -1,11 +1,8 @@
-import { IError, ITelaRota, IV6Menu, IV6Submenu, IV6Tela } from 'ngx-sp-infra';
+import { IError, MenuSubmenuTela } from 'ngx-sp-infra';
 
 export class RetTelasPesquisa implements IError {
   Error: boolean = false;
   ErrorMessage: string = "";
 
-  Telas: ITelaRota[] = [];
-  _Telas: IV6Tela[] = [];
-  Menus: IV6Menu[] = [];
-  Submenus: IV6Submenu[] = [];
+  MenuSubmenuTela: MenuSubmenuTela = new MenuSubmenuTela();
 }
