885ef4245793574006c46d15ec2514664ac9eb40
Erick Carvalho <erickzcarvalho25@outlook.com>
2025-11-13 18:20:23 -0300

    ngx-sp-auth | v4.3.14 | Commit automático
    Ajuste nos componentes de submenu para não chamar WS caso estejam na Documentação V6

--- projects/ngx-sp-auth/src/lib/widgets/sub-menu-card/sub-menu-card.component.ts
+++ projects/ngx-sp-auth/src/lib/widgets/sub-menu-card/sub-menu-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 import { LibIconsComponent } from 'ngx-sp-infra';
-import { MenuServicesService } from '../../../public-api';
+
+import { MenuServicesService } from '../../components/menu-lateral/menu-services.service';
+import { LibCustomEnvironmentService } from '../../custom/lib-custom-environment.service';
 import { ProjectUtilservice } from '../../project/project-utils.service';
 
 export class NavSubmenuCards {
@@ -9,67 +12,79 @@ export class NavSubmenuCards {
   descricao: string = '';
   urlPath: string = '';
   isExternal:boolean = false;
-};
+}
 
 @Component({
   selector: 'sub-menu-card, lib-submenu-card',
-  imports: [LibIconsComponent],
+  imports: [ LibIconsComponent ],
   template: `
     <div class="max-card-menu row">
       @for(card of subMenuCards; track $index) {
-        <a href="{{ card.isExternal ? GetExternalUrl(card.urlPath) : card.urlPath }}" class="card-link col-4" target="_blank">
-          <div class="card">
+        <a [href]="card.isExternal ? getExternalUrl(card.urlPath) : card.urlPath" target="_blank" class="card-link col-4">
+          <div class="card w-100 border-0 rounded text-center glb-cursor-pointer">
             <div class="card-icon">
               <div class="card-icon2">
-                <lib-icon
-                  class="bold"
-                  iconName="{{ card.icon ? card.icon : 'engrenagem'}}"
-                  iconColor="blue"
-                  [iconFill]="true"
-                  [iconSize]="35"
-                ></lib-icon>
+                <lib-icon class="bold" [iconName]="card.icon ? card.icon : 'engrenagem'" iconColor="blue" [iconSize]="35" />
               </div>
             </div>
+
             <h3 class="card-title">{{ card.titulo }}</h3>
-            <p class="card-text" title="{{ card.descricao }}">
-              {{ card.descricao }}
-            </p>
+            <p class="card-text" [title]="card.descricao"> {{ card.descricao }} </p>
           </div>
         </a>
       }
     </div>
   `,
-  styleUrl: './sub-menu-card.component.scss',
-  standalone: true
+  styleUrl: './sub-menu-card.component.scss'
 })
 export class SubMenuCardComponent implements OnInit {
-GetExternalUrl(url: string) {
-    return `${ this.hostName == "" ? `https://${ window.location.host }` : this.hostName }/${ url }`;
+
+  // #region ==========> PROPERTIES <==========
+
+  // #region PUBLIC
+  @Input() subMenuCards: NavSubmenuCards[] = [];
+
+  public hostName:string = "";
+
+  public getExternalUrl = (url: string): string => { return `${ this.hostName == "" ? `https://${ window.location.host }` : this.hostName }/${ url }` }
+  // #endregion PUBLIC
+
+  // #endregion ==========> PROPERTIES <==========
+
+
+  constructor(
+    private _menuService: MenuServicesService,
+    private _projectUtil: ProjectUtilservice,
+
+    private _customEnv: LibCustomEnvironmentService
+  ) { }
+
+  ngOnInit(): void {
+    this.GetHostName();
+  }
+
+
+  // #region ==========> API METHODS <==========
+
+  // #region GET
+  
+  public GetHostName() {
+    // Validação adicionada para ignorar esta chamada caso o componente seja exibido na docs
+    if (this._customEnv.product === "InfraCodeDocs") return;
+
+    this._menuService.GetHostServerOutSystems().subscribe({
+      next:response => this.hostName = response.String,
+      error:error => this._projectUtil.showHttpError(error)
+    })
+  }
+
+  // #endregion GET
+
+  // #endregion ==========> API METHODS <==========
+
+
+  // #region ==========> UTILS <==========
+  // [...]
+  // #endregion ==========> UTILS <==========
 
 }
-
-ngOnInit(): void {
-  this.GetHostName();
-}
-
-/**
- *
- */
-constructor(private _menuService:MenuServicesService, private _projectUtil:ProjectUtilservice){}
-
-
-public hostName:string = "";
-
-@Input() subMenuCards: NavSubmenuCards[] = [];
-
-
-public GetHostName() {
-  this._menuService.GetHostServerOutSystems().subscribe({
-    next:response => this.hostName = response.String,
-    error:error => this._projectUtil.showHttpError(error)
-  })
-}
-
-
-}
-
